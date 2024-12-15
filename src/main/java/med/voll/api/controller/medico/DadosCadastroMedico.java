888102a456954615f16ae4108c8b7153e513cfea
igorgc <igor.galeno@outlook.com>
2024-12-15 09:20:48 -0300

    Aplicando regra de validação com o BeamValidation

--- src/main/java/med/voll/api/controller/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/controller/medico/DadosCadastroMedico.java
@@ -1,12 +1,26 @@
 package med.voll.api.controller.medico;
 
+import jakarta.persistence.Embedded;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.controller.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //Expressao regular onde o d se refere a digito e entre chaves de quanto a quanto caracter ele pode receber no caso de 4 a 6 char
         String crm,
+        @NotNull
         Especialidade especialidade,
+        @NotNull
+        @Valid //Validar também no outro Record os dados com o BeamValidation
         DadosEndereco endereco
 ) {
 }
