fef90572d9eded44e0e8226b5ef089819174e915
igorgc <igor.galeno@outlook.com>
2024-12-15 08:53:43 -0300

    Persistencia dos dados no BD

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
 import med.voll.api.controller.medico.DadosCadastroMedico;
+import med.voll.api.controller.medico.Medico;
+import med.voll.api.controller.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
-    public void cadastro(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
+
     }
-
-
-
-
 
 
 }
