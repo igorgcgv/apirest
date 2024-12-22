ecce99df1f73d6e1784e79f2291c76df5b86fab2
igorgc <igor.galeno@outlook.com>
2024-12-22 00:24:20 -0300

    Aplicando as boas praticas dos protocolos HTTP utilizando a classe "ResponseEntity"!

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,21 +19,25 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page);
     }
 
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
         var medico = repository.getReferenceById(dados.id()); //Carregar os dados do banco de dados pelo ID
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
 
     }
 
@@ -45,9 +50,11 @@ public class MedicoController {
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void ecluix(@PathVariable Long id) {
+    public ResponseEntity ecluix(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
 
 
     }
---
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -0,0 +1,29 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosDetalhamentoMedico(
+        Long id,
+        String nome,
+        String email,
+        String telefone,
+        String crm,
+        Especialidade especialidade,
+        Endereco endereco,
+        boolean ativo
+) {
+
+    public DadosDetalhamentoMedico(Medico medico) {
+        this(
+                medico.getId(),
+                medico.getNome(),
+                medico.getEmail(),
+                medico.getTelefone(),
+                medico.getCrm(),
+                medico.getEspecialidade(),
+                medico.getEndereco(),
+                medico.getAtivo())
+        ;
+
+    }
+}
