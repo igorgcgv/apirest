ae73ff829cd07ba329ab8bbe677c380643896689
igorgc <igor.galeno@outlook.com>
2025-01-11 20:22:02 -0300

    Implementação das Classes de Validação utilizando uma interface!

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -2,12 +2,16 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-public class ValidadorHorarioAntecedencia {
+@Service
+public class ValidadorHorarioAntecedencia  implements ValidadorAgendamentoDeConsulta{
 
+    @Autowired
     public void validar (DadosAgendamentoConsulta dados){
 
         var dataConsulta = dados.data();
--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -2,11 +2,15 @@ package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 
-public class ValidadorHorarioFuncionamentoClinica {
+@Service
+public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendamentoDeConsulta{
 
+    @Autowired
     public void validar (DadosAgendamentoConsulta dados) {
 
         var dataConsulta = dados.data();
